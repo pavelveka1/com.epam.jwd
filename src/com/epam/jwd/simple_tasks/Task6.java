1440c4e6095f6fb2ba0b1bba8059a65b2491897b
Pavel <pavelveka100@gmail.com>
2020-06-02 20:45:11 +0300

    Version 2.1

--- src/com/epam/jwd/simple_tasks/Task6.java
+++ src/com/epam/jwd/simple_tasks/Task6.java
@@ -10,20 +10,53 @@ public class Task6 {
 	public static int calculateIterationOutOfRangeInteger(int a1, int step) {
 		int sum = 0;
 		int n = 1;
-		while ((Integer.MAX_VALUE - sum) > (a1 + step * (n - 1))) {
-			sum = (a1 + a1 + step * n) * (n / 2);
-			n++;
+		int nextValue = 0;
+
+		while (true) {
+			nextValue = a1 + step * (n - 1);
+
+			if (nextValue < 0) {
+				if ((Integer.MIN_VALUE + Math.abs(sum)) < nextValue) {
+					sum = (a1 + a1 + step * (n - 1)) * (n / 2);
+					n++;
+				} else {
+					return ++n;
+				}
+			} else {
+				if ((Integer.MAX_VALUE - Math.abs(sum)) >= nextValue) {
+					sum = (a1 + a1 + step * (n - 1)) * (n / 2);
+					n++;
+				} else {
+					return ++n;
+				}
+			}
 		}
-		return ++n;
 	}
 
 	public static long calculateIterationOutOfRangeLong(long a1, long step) {
 		long sum = 0;
 		long n = 1;
-		while ((Long.MAX_VALUE - sum) > (a1 + step * (n - 1))) {
-			sum = (a1 + a1 + step * n) * (n / 2);
-			n++;
+		long nextValue = 0;
+
+		while (true) {
+			nextValue = a1 + step * (n - 1);
+
+			if (nextValue < 0) {
+
+				if ((Long.MIN_VALUE + Math.abs(sum)) < nextValue) {
+					sum = (a1 + a1 + step * (n - 1)) * (n / 2);
+					n++;
+				} else {
+					return ++n;
+				}
+			} else {
+				if ((Long.MAX_VALUE - Math.abs(sum)) >= nextValue) {
+					sum = (a1 + a1 + step * (n - 1)) * (n / 2);
+					n++;
+				} else {
+					return ++n;
+				}
+			}
 		}
-		return ++n;
 	}
 }
